36493667b8dc9cdbd9e428499a687c1f9e3aa3f7
js923k <jstover@wayfair.com>
2020-04-03 16:34:23 -0500

    adds rooms and user ids

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+import {
+  ALERT_CHOOSE_ROOM,
+  ALERT_NEW_MESSAGE,
+  ALERT_NEW_USER,
+  ALERT_USERNAME_TAKEN,
+  INPUT_CHOOSE_ROOM,
+  INPUT_NEW_MESSAGE,
+  INPUT_GAME_START,
+} from "./constants";
 
 function App() {
+  const [response, setResponse] = useState({});
+  const [endpoint, setEndpoint] = useState("localhost:3001");
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("TestUser");
+  const [userList, setUserList] = useState([]);
+  const [messageList, setMessagesList] = useState([]);
+  const [roomID, setRoomId] = useState("");
+  const [tempRoomID, setTempRoomId] = useState("");
+  let messageInput;
+
+  const addToMessages = (msg) =>
+    setMessagesList((messageList) => [...messageList, msg]);
+
+  useEffect(() => {
+    const socket = socketIOClient(endpoint);
+    setSocket(socket);
+  }, []);
+
+  const sendMessage = () => {
+    // validity check @TODO:
+    socket.emit(INPUT_NEW_MESSAGE, { message });
+    messageInput.value = "";
+  };
+
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  const setRoom = () => {
+    setRoomId(tempRoomID);
+  };
+
+  const startGame = () => {
+    socket.emit(INPUT_GAME_START);
+  };
+
+  useEffect(() => {
+    if (socket) {
+      console.log("changeRoom");
+      socket.emit(INPUT_CHOOSE_ROOM, { roomID, username });
+      socket.on(ALERT_USERNAME_TAKEN, () => {
+        setRoomId(null);
+      });
+      socket.on(ALERT_NEW_MESSAGE, (message) => {
+        addToMessages(message);
+      });
+      socket.on(ALERT_NEW_USER, (users) => {
+        setUserList(users);
+      });
+    }
+  }, [roomID]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        DickStash {roomID && <h3>Room: {roomID}</h3>}
       </header>
+      <section className="sidebar">
+        <ul>{userList && userList.map((user) => <li>{user}</li>)}</ul>
+      </section>
+      <section>
+        {roomID && (
+          <section>
+            {messageList && (
+              <aside>
+                <h3>Press Start when all users are ready</h3>
+                <button onClick={startGame}>Start Game</button>
+              </aside>
+            )}
+          </section>
+        )}
+        {!roomID && (
+          <section>
+            <h1>Enter a room number & a username</h1>
+            <input
+              type="text"
+              placeholder="Room ID"
+              onChange={(e) => setTempRoomId(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <button onClick={setRoom}>Submit</button>
+          </section>
+        )}
+      </section>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+// Alerts 🔔
+export const ALERT_CHOOSE_ROOM = "alert_choose_room";
+export const ALERT_NEW_MESSAGE = "alert_new_message";
+export const ALERT_NEW_USER = "alert_new_user";
+export const ALERT_USERNAME_TAKEN = "alert_username_taken";
+
+// Input 📭
+export const INPUT_NEW_MESSAGE = "input_new_message";
+export const INPUT_CHOOSE_ROOM = "input_choose_room";
+export const INPUT_GAME_START = "input_game_start";
